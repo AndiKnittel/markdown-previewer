c8fd74cb40b0466119345b910f0c4f02f92915f0
Andreas Knittel <andiknittel.music@gmail.com>
2024-11-22 14:06:58 +0100

    full project minor tweaks

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import './App.css';
 import ReactMarkdown from 'react-markdown';
-import { marked } from 'marked';
+
 
 const defaultMarkdown = `
 # Welcome to my React Markdown Previewer!
@@ -39,10 +39,6 @@ There's also [links](https://freecodecamp.org), and
 
 ![freeCodeCamp Logo w/ Text](https://cdn.freecodecamp.org/testable-projects-fcc/images/fcc_secondary.svg)
 `;
-
-marked.setOptions({
-  breaks: true
-})
 
 function App() {
   const [markdownText, setMarkdownText] = useState<string>(defaultMarkdown)
